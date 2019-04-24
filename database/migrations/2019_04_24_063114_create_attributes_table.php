ab9399f8cca44a657e9a8f7b71bd05eb8c668a6b
Madhavi <Madhavi>
2019-04-24 12:09:33 +0530

    added foreign key attribute_set_id to attributes table, renamed attribute_set field to name in attribute_sets

---
+++ database/migrations/2019_04_24_063114_create_attributes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table-> string('attribute_name', 255);
+			$table-> string('default_value', 255);
+			$table->boolean('required');
+			$table->unsignedInteger('attribute_set_id');
+			
+			$table->foreign('attribute_set_id')->references('id')->on('attribute_sets');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attributes', function (Blueprint $table) {
+            //
+        });
+    }
+}
